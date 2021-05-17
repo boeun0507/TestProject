891c3efa15d8760d22e5e1171ca879c6f41a5c23
boeun0507 <82742755+boeun0507@users.noreply.github.com>
2021-05-17 13:00:38 +0900

    Update and rename Assets/Test1.cs to 자산/Test1.cs

--- 자산/Test1.cs
+++ 자산/Test1.cs
@@ -6,8 +6,8 @@ public class Test1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+   {
+   p(rin"t"t);
     }
 
     // Update is called once per frame
